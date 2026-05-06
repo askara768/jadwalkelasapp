1ff7e45f6d32f59892680f068e08ff23b0c5c38c
Mmanusia <mmanusia987@gmail.com>
2026-05-06 14:12:17 +0700

    uhh

---
+++ lib/auth/lovely_register_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LovelyRegisterScreen extends StatefulWidget {
+  const LovelyRegisterScreen({super.key});
+
+  @override
+  State<LovelyRegisterScreen> createState() => _LovelyRegisterScreenState();
+}
+
+class _LovelyRegisterScreenState extends State<LovelyRegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
