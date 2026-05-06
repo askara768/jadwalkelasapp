229b685676ef014e8f5c95023a5ab9a337d5d649
Mmanusia <mmanusia987@gmail.com>
2026-05-06 12:55:39 +0700

    iya

---
+++ lib/lovely_splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LovelySplashScreen extends StatefulWidget {
+  const LovelySplashScreen({super.key});
+
+  @override
+  State<LovelySplashScreen> createState() => _LovelySplashScreenState();
+}
+
+class _LovelySplashScreenState extends State<LovelySplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(Main());
@@ -14,6 +15,6 @@ class Main extends StatefulWidget {
 class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(debugShowCheckedModeBanner: false, home: ,);
   }
 }
