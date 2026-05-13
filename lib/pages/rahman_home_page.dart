324431293ce2472fc1d38f85f807ef668d148e4e
Mmanusia <mmanusia987@gmail.com>
2026-05-13 12:15:28 +0700

    uhh

--- lib/pages/rahman_home_page.dart
+++ lib/pages/rahman_home_page.dart
@@ -25,139 +25,209 @@ class _RahmanHomePageState extends State<RahmanHomePage> {
         ],
         title: Text("X RPL 1"),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {},
-            child: Container(
-              padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
-              height: 80,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset(""),
-                  SizedBox(width: 13),
-                  Text('X RPL 2'),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {},
-            child: Container(
-              padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
-              height: 80,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset(""),
-                  SizedBox(width: 13),
-                  Text('X TKJ 1'),
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset("assets/images/img_2.png"),
+                    SizedBox(width: 13),
+                    Text('X TKR '),
+                  ],
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {},
-            child: Container(
-              padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
-              height: 80,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset(""),
-                  SizedBox(width: 13),
-                  Text('X TKJ 2'),
-                ],
-              ),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Container(
-              padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
-              height: 80,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset(""),
-                  SizedBox(width: 13),
-                  Text('X TKJ 3'),
-                ],
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset("assets/images/img.png"),
+                    SizedBox(width: 13),
+                    Text('X RPL '),
+                  ],
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {},
-            child: Container(
-              padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
-              height: 80,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset(""),
-                  SizedBox(width: 13),
-                  Text('X TKJ 4'),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {},
-            child: Container(
-              padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
-              height: 80,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset(""),
-                  SizedBox(width: 13),
-                  Text('X RPL 2'),
-                ],
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset("assets/images/img_1.png"),
+                    SizedBox(width: 13),
+                    Text('X TKJ '),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset("assets/images/img_3.png"),
+                    SizedBox(width: 13),
+                    Text('X TSM '),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset("assets/images/img_4.png"),
+                    SizedBox(width: 13),
+                    Text('X KPR '),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset("assets/images/img_5.png"),
+                    SizedBox(width: 13),
+                    Text('X ANM '),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset("assets/images/img_7.png"),
+                    SizedBox(width: 13),
+                    Text('X DKV '),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset("assets/images/img_8.png"),
+                    SizedBox(width: 13),
+                    Text('X SIJA '),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset("assets/images/img_9.png"),
+                    SizedBox(width: 13),
+                    Text('X DPIB '),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
 
       // Container(
